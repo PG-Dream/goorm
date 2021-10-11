ea1201b9ac21eb30fe6ad0c425a86304a7a41c50
Limseunghyun95 <limseunghyun95@gmail.com>
2021-10-11 08:52:35 +0000

    [lim] update 43173.py

---
+++ lim_43173.py
@@ -0,0 +1,6 @@
+# source : https://level.goorm.io/exam/43173/%EB%B9%84%ED%8A%B8%EC%97%B0%EC%82%B0-%EA%B8%B0%EB%B3%B8-2/quiz/1
+
+user_input = list(map(int, input().split(" ")))
+
+print(user_input[0] >> user_input[1])
+print(user_input[0] << user_input[1])
