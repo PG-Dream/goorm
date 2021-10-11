fbb9fa4b17f3fb83ed9d843205e2ca2041189c82
Limseunghyun95 <limseunghyun95@gmail.com>
2021-10-11 09:35:56 +0000

    [lim] update 43092.py

---
+++ lim_43092.py
@@ -0,0 +1,5 @@
+# https://level.goorm.io/exam/43092/%EB%A6%AC%EA%B7%B8-%EA%B2%BD%EA%B8%B0-%ED%9A%9F%EC%88%98-%EA%B5%AC%ED%95%98%EA%B8%B0/quiz/1
+
+user_input = int(input())
+
+print(sum([i for i in range(1, user_input)]))
